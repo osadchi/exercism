e91ab1938cebe53447d489f5d711631e8a29765a
osieman <swcalc@gmail.com>
2025-04-25 03:48:30 +0300

    йобинбобин

---
+++ src/main/java/org/example/NucleotideCounter.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class NucleotideCounter {
+
+    NucleotideCounter(String sequence) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    Map<Character, Integer> nucleotideCounts() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
